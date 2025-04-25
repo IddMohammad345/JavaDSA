4ff79c2f5ff981821b671ce47d262b1c470b07df
Idd mohammad <mohammadidd345@gmail.com>
2025-04-25 11:11:14 +0530

    java DSA Bubble Sorting

--- DSA_Java/JavaDSA/src/Bubble_Sort.java
+++ DSA_Java/JavaDSA/src/Bubble_Sort.java
@@ -1,18 +1,25 @@
+import java.util.Arrays;
+
 public class Bubble_Sort {
     public static void main(String[] args) {
 
         int[] arr={4,5,63,2,60,45,23,9,44,20,98,11,3};
         for (int j=0;j<arr.length-1;j++) {
-            for (int i = 0; i < arr.length-1; i++) {
+            for (int i = 0; i < arr.length-1-j; i++) {
                 if (arr[i] > arr[i + 1]) {
-                    int temp = arr[i];   //temp=4;arr[i]=4;
-                    arr[i] = arr[i+1];    //arr[i+1]=4
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
                     arr[i+1] = temp;
                 }
             }
         }
-        for (int i = 0; i < arr.length-1; i++) {
+        //printing using normal core java
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" , ");
         }
+
+        System.out.println("\n=====================================================");
+        Arrays.stream(arr)
+                .forEach(i->System.out.print(i+" , "));
     }
 }
