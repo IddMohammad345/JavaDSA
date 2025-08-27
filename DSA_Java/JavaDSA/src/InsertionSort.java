25ee70069985007fd94db19a1c3ff9c678d87caf
Idd mohammad <mohammadidd345@gmail.com>
2025-08-27 21:23:03 +0530

    Insertion Sort technique

--- DSA_Java/JavaDSA/src/InsertionSort.java
+++ DSA_Java/JavaDSA/src/InsertionSort.java
@@ -2,10 +2,10 @@ public class InsertionSort {
     public static void main(String[] args) {
         int arr[]= {5,6,2,3,1,8};
         for (int i=1;i<arr.length;i++){
-            int key=arr[i];
-            int j=i-1;
+            int key=arr[i];   //key=6
+            int j=i-1;        //j=0
             while (j>=0 && arr[j]>key){
-                arr[j+1]=arr[j];
+                arr[j+1]=arr[j];  //1 = 5
                 j--;
             }
             arr[j+1]=key;
